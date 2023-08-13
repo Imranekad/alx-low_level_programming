c2ec6050df895b7c57b3a2b1683c77414702c273
Imranekad <pilotestar@gmail.com>
2023-08-13 13:11:58 -0700

    ee

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *malloc_checked- allocate memory
+ * @b: insigned
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pt;
+	pt = malloc(b);
+	if (pt == NULL)
+		exit(98);
+	return(pt);
+}	

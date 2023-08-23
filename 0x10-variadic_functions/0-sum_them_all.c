940cfae6625b090a28068ee52fdbbbbea8f3fb68
Imranekad <pilotestar@gmail.com>
2023-08-23 14:03:27 -0700

    ee

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,22 @@
-#include<stdarg.h>
-/**sum_them_all- return the sum of intigers
- * @count: intiger
- * return s
- */
+#include <stdarg.h>
 
+/** sum_them_all- return sum of integers
+ *@n: number of integers
+ *Return s: sum of the integers
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	if(n == 0)
+	if (n == 0) 
+	{
 		return 0;
+	}
 	va_list args;
 	va_start(args, n);
 	int s = 0;
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 	{
 		int num = va_arg(args, int);
-		s = s + num;
-	}
-	va_end(args);
-	return s;
+		s += num;
+		va_end(args);
+		return s;
 }
